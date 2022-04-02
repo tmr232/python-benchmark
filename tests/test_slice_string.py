1af8ce9f03258521ad22a885348ff68ce0901eb3
Tamir Bahar <tamir.bahar@gmail.com>
2022-04-02 17:04:04 +0300

    Add another option to the slice-string tests.

--- tests/test_slice_string.py
+++ tests/test_slice_string.py
@@ -1,3 +1,4 @@
+import operator
 import re
 
 
@@ -18,7 +19,7 @@ class TestPairs3:
         def split(text):
             return text[0:2], text[2:4], text[4:6]
 
-        benchmark(split, self.TEXT)
+        benchmark.pedantic(split, args=(self.TEXT,), iterations=50, rounds=20000)
 
     def test_pairs3_re(self, benchmark):
         """Split the string using regex"""
@@ -26,7 +27,7 @@ class TestPairs3:
         def split(text, _match=re.compile(r"(..)(..)(..)").match):
             return _match(text).groups()
 
-        benchmark(split, self.TEXT)
+        benchmark.pedantic(split, args=(self.TEXT,), iterations=50, rounds=20000)
 
     def test_pairs3_slice_cached(self, benchmark):
         """Split the string using cached slices"""
@@ -34,4 +35,15 @@ class TestPairs3:
         def split(text, group1=slice(0, 2), group2=slice(2, 4), group3=slice(4, 6)):
             return text[group1], text[group2], text[group3]
 
-        benchmark(split, self.TEXT)
+        benchmark.pedantic(split, args=(self.TEXT,), iterations=50, rounds=20000)
+
+    def test_pairs3_cached_itemgetter(self, benchmark):
+        """Split the string using a cached itemgetter"""
+
+        def split(
+            text,
+            _split=operator.itemgetter(slice(0, 2), slice(2, 4), slice(4, 6)),
+        ):
+            return _split(text)
+
+        benchmark.pedantic(split, args=(self.TEXT,), iterations=50, rounds=20000)
